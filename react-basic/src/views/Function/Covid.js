1b80bd13c1d0eece37c197519d1f20713c369143
Ta Van Cuong <tavancuong4444@gmail.com>
2023-02-15 16:22:33 +0700

    update project

--- react-basic/src/views/Function/Covid.js
+++ react-basic/src/views/Function/Covid.js
@@ -27,6 +27,7 @@ function Covid() {
       }
     })();
   }, []);
+
   //   useEffect(async () => {
   //     let res = await axios.get(
   //       "https://api.covid19api.com/country/vietnam?from=2021-10-01T00%3A00%3A00Z&to=2021-10-20T00%3A00%3A00Z"
